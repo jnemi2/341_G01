37c3ed787471550f9fbac9b710fab3533a2f0276
bauti <jnemi@fie.undef.edu.ar>
2021-09-06 18:07:08 -0300

    ADD - Assignment 5

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+import vector2.vector2 as vec
+
+
+data = input("Ingrese un vector en 2D en formato <num>,<num>: ")
+data = data.split(",")
+data[0] = float(data[0].strip())
+data[1] = float(data[1].strip())
+
+while data[0] != 0 or data[1] != 0:
+    print("La distancia al orígen del punto ({}, {}) es: {}".format(
+        data[0], data[1], vec.norm(data[0], data[1])
+    ))
+    data = input("Ingrese un vector en 2D en formato <num>,<num>: ")
+    data = data.split(",")
+    data[0] = float(data[0].strip())
+    data[1] = float(data[1].strip())
+
---
+++ vector2/vector2.py
@@ -0,0 +1,5 @@
+import math
+
+
+def norm(x, y):
+    return math.sqrt(x**2 + y**2)
