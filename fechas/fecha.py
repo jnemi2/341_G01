aff3d8a22c7b4801d78dd8cbdcca699d3eedd77d
bauti <jnemi@fie.undef.edu.ar>
2021-09-06 17:38:07 -0300

    ADD - Assignment 1

---
+++ fechas/fecha.py
@@ -0,0 +1,5 @@
+
+def crear_mes(nombre, abreviatura, cantidad_dias):
+    print("Se creó el mes ", nombre, ", que se abrevia ", abreviatura,
+          " y contiene ", cantidad_dias, " días.")
+
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello world")
+import fechas.fecha
+
+
+fechas.fecha.crear_mes("Enero", "ENE", 31)
