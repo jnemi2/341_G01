7c0679e72505bc982c2dc0cf6811c8fb0c714ea9
bauti <jnemi@fie.undef.edu.ar>
2021-09-06 18:10:16 -0300

    REF - Assignment 5 input request

--- vector2/vector2.py
+++ vector2/vector2.py
@@ -1,5 +1,13 @@
 import math
 
 
+def ask_for_vector():
+    data = input("Ingrese un vector en 2D en formato <num>,<num>: ")
+    data = data.split(",")
+    data[0] = float(data[0].strip())
+    data[1] = float(data[1].strip())
+    return data
+
+
 def norm(x, y):
     return math.sqrt(x**2 + y**2)
