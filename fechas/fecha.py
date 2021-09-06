312bd24959f17594eb67b315f88c49749d7249a4
bauti <jnemi@fie.undef.edu.ar>
2021-09-06 17:39:29 -0300

    ADD - Assignment 1 doc

--- fechas/fecha.py
+++ fechas/fecha.py
@@ -1,5 +1,11 @@
 
 def crear_mes(nombre, abreviatura, cantidad_dias):
+    """
+
+    :param nombre: nombre del mes
+    :param abreviatura: abreviatura del mes. ej: ENE
+    :param cantidad_dias: cantidad de dias del mes
+    """
     print("Se creó el mes ", nombre, ", que se abrevia ", abreviatura,
           " y contiene ", cantidad_dias, " días.")
 
