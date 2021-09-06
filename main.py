c8c27e059941bb2fc408f17be3d8c2cd5b716348
bauti <jnemi@fie.undef.edu.ar>
2021-09-06 17:55:25 -0300

    ADD - Assignment 4

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+
+
+date = input("Ingrese una fecha en formato dd/mm/aaaa: ")
+date = date.split("/")
+print("El día ingresado fue: ", date[0])
+print("El mes ingresado fue: ", date[1])
+print("El año ingresado fue: ", date[2])
